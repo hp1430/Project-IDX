d12279c2a6b23def6a445a2c738e23516a68a64f
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-07-12 18:27:30 +0530

    update websockets url

--- frontend/src/pages/ProjectPlayground.jsx
+++ frontend/src/pages/ProjectPlayground.jsx
@@ -35,7 +35,7 @@ export const ProjectPlayground = () => {
             });
 
             try {
-                const ws = new WebSocket(`${import.meta.env.VITE_WEBSOCKET_URL}/terminal?projectId=`+projectIdFromUrl);
+                const ws = new WebSocket(`${import.meta.env.VITE_WEBSOCKET_URL}?projectId=`+projectIdFromUrl);
             //    editorSocket.emit("getPort", { containerName: projectIdFromUrl });
                 setTerminalSocket(ws);
             }
